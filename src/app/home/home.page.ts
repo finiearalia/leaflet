adbc480a14f29991a28373eba3a2fbad0df1d75a
finiearalia <finiearaliashearani@mail.ugm.ac.id>
2024-09-23 10:23:02 +0700

    PGPBL 06

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  map! : L.Map;
+  marker!: L.Marker;
+
   constructor() {}
+  ngOnInit() {
+
+  }
+
+  ionViewDidEnter() {
+    this.map = L.map('mapId').setView([-6.175253728264677, 106.82706696492357], 13);
+    this.marker = L.marker([-6.175253728264677, 106.82706696492357]).addTo(this.map);
+    this.marker.bindPopup('Ini Monas').openPopup();
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+  }
+
 
 }
